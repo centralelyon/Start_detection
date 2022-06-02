18a31561be2ad73315cb08588f6004f1a1d0e723
Cyvernes <101457597+Cyvernes@users.noreply.github.com>
2022-06-02 13:46:48 +0200

    Add files via upload

--- Start_detection_audio.py
+++ Start_detection_audio.py
@@ -15,6 +15,42 @@ import json
 
 import matplotlib.pyplot as plt
 import cupy as cp
+from scipy.signal import butter, lfilter, freqz
+
+import time
+
+#filtrage numérique
+
+def butter_lowpass(cutoff, fs, order=3):#filtre passe bas
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_bandpass(cutoff1,cutoff2,fs,order = 3):#filtre passe bande
+    nyq = 0.5*fs
+    b,a = butter(order,(cutoff1/nyq,cutoff2/nyq),"bandpass")
+    return b,a
+
+def butter_bandpass_filter(data,cutoff1,cutoff2,fs,order = 3): #pour appliquer le filtre
+    b,a = butter_bandpass(cutoff1,cutoff2,fs,order = order)
+    y = lfilter(b,a,data)
+    return(y)
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):#pour appliquer le filtrage
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def spectre(signal,fs = 44100):#dessine le spectre
+    T = len(signal)/fs
+    tfd = np.fft.fft(signal)
+    N=len(signal)
+    spectre = np.absolute(tfd)*2/N
+    freq=np.arange(N)*1.0/T
+    plt.plot(freq,spectre)
+    plt.axis([-0.1,fs/2,0,spectre.max()])
 
 def get_index(list_dict, vid_name):
     """helper to read the json file."""
@@ -77,16 +113,83 @@ def extract_time_start(video_path, bip_ref_path="ref_bip_isolated.wav", referenc
     return start
 
 
-def extract_time_start_intercorr(video_path, bip_ref_path="ref_bip_isolated.wav"):
-    fs, s_ref = aIO.read_audio_file(bip_ref_path)
-        
-    audioclip = mp.AudioFileClip(video_path)
-    s_long = audioclip.to_soundarray(fps = fs)[:,0]
-    intercorr = cp.correlate(cp.array(s_long),cp.array(s_ref))
-    return(cp.argmax(intercorr)/fs +0.11)#on ajoute les 0.11 seconde de bkanc. On pourrait être plus précis au regard de la précision de l'intercorr
-        
+def continuous_normalisation(signal,windows):
+    kernel = cp.ones(windows)/windows
+    mean = cp.convolve(signal,kernel,"same")
+    carre = (signal - mean)**2
 
+    std = cp.sqrt(cp.convolve(carre,kernel,"same"))
+    std[std==0] = 1
+    rep = signal/std
+    
+    return(rep)
+
+def extract_time_start_intercorr(video_path, bip_ref_path="ref_bip_isolated.wav"):
+    
+    fs, s_ref = aIO.read_audio_file(bip_ref_path)
+
+    s_ref = s_ref[4851:-1]#on enleve le bruit blanc
+    s_ref = butter_bandpass_filter(s_ref,870, 1100, fs, order=3)
+    audioclip = mp.AudioFileClip(video_path)
+    
+    signal = audioclip.to_soundarray(fps = fs)[:,0]
+    #plt.plot(np.arange(0,len(signal))/fs,signal)
+    #plt.title('signal')
+    
+    #filtrage du signal
+    signal = butter_bandpass_filter(signal,870, 1100, fs, order=3)
+    #plt.plot(np.arange(0,len(signal))/fs,signal)
+    
+    #passage sur carte graphique
+    
+    signal= cp.array(signal)
+
+    #signal =  continuous_normalisation(signal,3*len(s_ref))
+    #plt.figure()
+    #plt.plot(np.arange(0,len(signal))/fs,signal.get())
+    #plt.title("apres normalisation continue")
+
+
+    intercorr = cp.correlate(signal,cp.array(s_ref))
+    #plt.figure()
+    #plt.plot(np.arange(0,len(intercorr))/fs,intercorr.get())
+    time_start = cp.argmax(intercorr,axis=0)/fs #on ajoute les 0.11 seconde de bkanc. On pourrait être plus précis au regard de la précision de l'intercorr
+    
+    return(float(time_start)) #la récupération de time_start en float ou en numpy.ndarray est extrèmement lente. Je ne comprends pas
+
+def extract_time_start_naif(video_path,bip_ref_path="ref_bip_isolated.wav"):
+    fs, s_ref = aIO.read_audio_file(bip_ref_path)
+    s_ref = s_ref[4851:-1]#on enleve le bruit blanc
+    audioclip = mp.AudioFileClip(video_path)
+    fs = 44100
+    signal = audioclip.to_soundarray(fps = fs)[:,0]
+    
+    signal =  continuous_normalisation(cp.array(signal),3*len(s_ref)).get()
+    
+    signal = butter_bandpass_filter(signal,870, 1100, fs, order=3)
+    
+    
+    return(np.argmax(np.absolute(signal))/fs)
+    
+def synchro_videos(video_path1,video_path2):
+    fs = 44100
+    
+    audioclip1 = mp.AudioFileClip(video_path1)
+    signal1 = audioclip1.to_soundarray(fps = fs)[:,0]
+    
+    audioclip2 = mp.AudioFileClip(video_path2)
+    signal2 = audioclip2.to_soundarray(fps = fs)[:,0]
+    
+    if len(signal1) >= len(signal2):
+        return(float(cp.argmax(cp.correlate(cp.array(signal1),cp.array(signal2)))/fs))
+    else:
+        return(float(cp.argmax(cp.correlate(cp.array(signal2),cp.array(signal1)))/fs))
 if __name__ == "__main__":
-    video_path ="./vidéos/2021_Montpellier_freestyle_hommes_50_FinaleC_fixeDroite.mp4"
+    """
+    video_path ='./data/2021_GT_Marseille/2021_Marseille_dos_dames_50_serie3/2021_Marseille_dos_dames_50_serie3_fixeDroite.mp4'
     bip_ref_path="ref_bip_isolated.wav"
-    print(extract_time_start_intercorr(video_path))
+    print(extract_time_start_naif(video_path))
+    """
+    video_path1 ='./data/2021_GT_Marseille/2021_Marseille_freestyle_hommes_50_serie5/2021_Marseille_freestyle_hommes_50_serie5_fixeDroite.mp4'
+    video_path2 ='./data/2021_GT_Marseille/2021_Marseille_freestyle_hommes_50_serie5/2021_Marseille_freestyle_hommes_50_serie5_fixeGauche.mp4'
+    print(synchro_videos(video_path2,video_path1))
